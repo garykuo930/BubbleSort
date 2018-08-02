db36ab25bd5cd070dad67b774bf4734e4a703198
ji3g4vul3eji <41719417+ji3g4vul3eji@users.noreply.github.com>
2018-08-02 17:10:13 +0800

    Initial commit

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Sort = new int[5] { 155, 56, 10, 15, 6 };
+
+            for (int i = 0; i < Sort.Length - 1; i += 1)
+            {
+                for (int j = 0; j < Sort.Length - 1 - i; j += 1)
+                {
+                    if (Sort[j] > Sort[j + 1])
+                    {
+                        int cache = Sort[j];
+                        Sort[j] = Sort[j + 1];
+                        Sort[j + 1] = cache;
+                    }
+                }
+            }
+            for (int i = 0; i < Sort.Length; i++)
+            {
+                Console.WriteLine(Sort[i]);
+            }
+        }
+    }
+}
